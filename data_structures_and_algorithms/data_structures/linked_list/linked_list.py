e255953ffcc949a5e690e4be6f12fbdf86a9dea8
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-08-23 23:59:55 +0300

    linked-list edited

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,55 @@
-class LinkedList:
+class Node():
+    def __init__(self, info):
+        self.info = info
+        self.next = None
+
+class LinkedList():
     """
     Put docstring here
     """
 
     # put your LinkedList implementation here
-    pass
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+    
+    def insert(self, info):
+        new_node = Node(info)
+        new_node.next = self.head
+        self.head = new_node
+
+    def includes(self, info):
+        current = self.head
+        while current:
+            if info == current.info:
+                print('True')
+                return True
+            else: current = current.next
+        print('False')
+        return False
+
+    def __str__(self):
+        current = self.head
+        output = ''
+        while current: 
+            output += f'{current.info} -> '
+            current = current.next
+        output += f'{current}'
+        return output
+
+if __name__=='__main__':
+    drinks = LinkedList()
+    a = drinks.append("Coffee")
+    drinks.append("Ice_Tea")
+    drinks.append("Lemonade")
+    print(f'{drinks}')
+
--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,51 @@
 from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
     LinkedList,
 )
+import pytest
 
 
 def test_instance():
     ll = LinkedList()
     assert isinstance(ll, LinkedList)
+
+@pytest.fixture
+def prepare_info():
+    drinks = LinkedList()
+    a = drinks.append("Coffee")
+    b = drinks.append("Ice_Tea")
+    c = drinks.append("Lemonade")
+    d = drinks.includes("Mocha")
+    return {"a":a,"b":b,"c":c,"d":d,"e":e,"drinks":drinks}
+
+def first_test():
+    drinks = LinkedList()
+    drinks.append("Coffee")
+    drinks.append("Ice_Tea")
+    drinks.includes("Lemonade")
+    expected = 'Coffee -> Ice_Tea -> Lemonade -> None'
+    actual = drinks.__str__()
+    assert expected == actual
+
+def second_test(prepare_info):
+    prepare_info['drinks']
+    prepare_info['a']
+    prepare_info['b']
+    prepare_info['c']
+    expected = True
+    actual = prepare_info['drinks'].includes("Lemonade")
+    assert expected == actual
+
+def third_test(prepare_info):
+    prepare_info['drinks']
+    prepare_info['a']
+    prepare_info['b']
+    prepare_info['c']
+    expected = False
+    actual = prepare_info['drinks'].includes("Milk")
+    assert expected == actual
+
+def fourth_test(prepare_info):
+    new_book = prepare_info['drinks'].insert('Milk')
+    expected = 'Milk -> History -> Horror -> Novel -> None'
+    actual = prepare_info['drinks'].__str__()
+    assert expected == actual
