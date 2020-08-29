9f4106d66eb256f6c85da3c0af4732aecd52bd51
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-08-29 14:57:58 +0300

    stack-and-queue

---
+++ data_structures_and_algorithms/data_structures/stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,96 @@
+class Node():
+    def __init__(self, info):
+        self.info = info
+        self.next = None
+
+class Stack():
+    def __init__(self):
+        self.top = None
+        self.items = []
+
+    def push(self, info):
+        if self.top == None:
+            self.top = Node(info)
+            self.items.append(info)
+        else:
+            new_node = Node(info)
+            self.top.next = new_node
+            self.top = new_node
+            self.items.append(new_node.info)
+
+    def pop(self):
+        try: 
+            popped = self.top
+            self.top = self.top.next
+            popped.next = None
+            return popped.info
+        except AttributeError as error:
+            return 'the stack is empty'
+
+    def peek(self):
+        try:
+            return self.top.info
+        except AttributeError as error:
+            return 'the stack is empty'
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+class Queue():
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.items = []
+
+    def enqueue(self, info):
+        new_node = Node(info)
+        if not self.front and not self.rear:
+            self.front = new_node
+            self.rear = new_node
+            self.items.append(new_node.info)
+            return
+        old_rear = self.rear
+        self.rear = new_node
+        self.items.append(new_node.info)
+
+    def dequeue(self):
+        try:
+            current = self.front
+            self.front = current.next
+            if self.front == None:
+                self.rear = None
+        except AttributeError as error:
+            return 'the queue is empty'
+
+    def peek(self):
+        try:
+            return self.front.info
+        except AttributeError as error:
+            return 'the queue is empty'
+
+    def is_empty(self):
+       if not self.front:
+           return True
+       else :
+           return False
+
+if __name__ == '__main__':
+    fruites = Stack()
+    fruit = Queue()
+    fruites.push('Orange')
+    fruites.push('Pineapple')
+    fruites.push('Berry')
+    print(fruites.items)
+    print(fruites.peek())
+    print(fruites.pop())
+    fruit.enqueue('Mango')
+    fruit.enqueue('Strawberry')
+    fruit.enqueue('Peach')
+    print(fruit.items)
+    print(fruit.front.info)
+    print(fruit.rear.info)
+    print(fruit.peek())
+
---
+++ tests/data_structures/test_stacks_and_queues.py
@@ -0,0 +1,64 @@
+from data_structures_and_algorithms.data_structures.stacks_and_queues.stacks_and_queues import Node, Stack, Queue
+
+def test_one_stack():
+    fruites = Stack()
+    fruites.push('Orange')
+    fruites.push('Pineapple')
+    fruites.push('Berry')
+    expected = ['Orange', 'Pineapple', 'Berry']
+    actual = fruites.items
+    assert expected == actual
+
+def test_two_stack():
+    fruites = Stack()
+    fruites.push('Orange')
+    fruites.push('Pineapple')
+    fruites.push('Berry')
+    expected = 'Berry'
+    actual = fruites.peek()
+    assert expected == actual
+
+def test_three_stack():
+    fruites = Stack()
+    fruites.push('Orange')
+    fruites.push('Pineapple')
+    fruites.push('Berry')
+    expected = 'Berry'
+    actual = fruites.pop()
+    assert expected == actual
+
+def test_one_queue():
+    fruit = Queue()
+    fruit.enqueue('Mango')
+    fruit.enqueue('Strawberry')
+    fruit.enqueue('Peach')
+    expected = ['Mango', 'Strawberry', 'Peach']
+    actual = fruit.items
+    assert expected == actual
+
+def test_two_queue():
+    fruit = Queue()
+    fruit.enqueue('Mango')
+    fruit.enqueue('Strawberry')
+    fruit.enqueue('Peach')
+    expected = 'Mango'
+    actual = fruit.front.info
+    assert expected == actual
+
+def test_three_queue():
+    fruit = Queue()
+    fruit.enqueue('Mango')
+    fruit.enqueue('Strawberry')
+    fruit.enqueue('Peach')
+    expected = 'Peach'
+    actual = fruit.rear.info
+    assert expected == actual
+
+def test_four_queue():
+    fruit = Queue()
+    fruit.enqueue('Mango')
+    fruit.enqueue('Strawberry')
+    fruit.enqueue('Peach')
+    expected = 'Mango'
+    actual = fruit.peek()
+    assert expected == actual
